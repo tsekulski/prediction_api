546d825468ccb1b192dcd4281d7d91d02c50947b
Tomek Sekulski <tomasz.sekulski@gmail.com>
2018-07-31 15:01:13 +0200

    draft user flow working e2e

--- prediction_api/predict.py
+++ prediction_api/predict.py
@@ -43,3 +43,31 @@ def generate():
             return redirect(url_for('predict.index'))
 
     return render_template('predict/generate.html')
+
+
+def get_prediction(id, check_user=True):
+    prediction = get_db().execute(
+        'SELECT p.id, prediction_value, prediction_input, created, user_id, username'
+        ' FROM predictions p JOIN users u ON p.user_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if prediction is None:
+        abort(404, "Prediction id {0} doesn't exist.".format(id))
+
+    if check_user and prediction['user_id'] != g.user['id']:
+        abort(403)
+
+    return prediction
+
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_prediction(id)
+    db = get_db()
+    db.execute('DELETE FROM predictions WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('predict.index'))
+
