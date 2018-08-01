0642310502af47edf268cf335b18ebd51aa6429f
Tomek Sekulski <tomasz.sekulski@gmail.com>
2018-08-01 14:50:12 +0200

    draft version of '/predict_from_json' endpoint

--- prediction_api/predict.py
+++ prediction_api/predict.py
@@ -1,10 +1,15 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, jsonify
 )
 from werkzeug.exceptions import abort
 
 from prediction_api.auth import login_required
 from prediction_api.db import get_db
+
+import json
+import requests
+import pandas as pd
+import numpy as np
 
 bp = Blueprint('predict', __name__)
 
@@ -34,17 +39,28 @@ def generate():
         if error is not None:
             flash(error)
         else:
-            input_variables = [var_1, var_2, var_3]
-            predicted_value = generate_prediction(input_variables)
-            db = get_db()
-            db.execute(
-                'INSERT INTO predictions (prediction_value, prediction_input, user_id)'
-                ' VALUES (?, ?, ?)',
-                (predicted_value, ' '.join(str(e) for e in input_variables), g.user['id'])
-                # input_variables must be converted to string before inserting into db
-            )
-            db.commit()
-            return redirect(url_for('predict.index'))
+           # input_variables = [var_1, var_2, var_3]
+            #predicted_value = generate_prediction(input_variables)
+            #db = get_db()
+            #db.execute(
+            #    'INSERT INTO predictions (prediction_value, prediction_input, user_id)'
+            #    ' VALUES (?, ?, ?)',
+            #    (predicted_value, ' '.join(str(e) for e in input_variables), g.user['id'])
+            #    # input_variables must be converted to string before inserting into db
+            #)
+            #db.commit()
+
+            col_names = ['var_1', 'var_2', 'var_3']
+            input_variables =  [[var_1], [var_2], [var_3]]
+            zipped = list(zip(col_names, input_variables))
+            prediction_data = dict(zipped)
+
+            #return redirect(url_for('predict.predict_from_json'))
+
+            res = requests.post('http://127.0.0.1:5000/predict_from_json', json=prediction_data)
+            return(res.json)
+
+            #return redirect(url_for('predict.index'))
 
     return render_template('predict/generate.html')
 
@@ -83,3 +99,23 @@ def generate_prediction(input_variables):
         predicted_value += variable
 
     return predicted_value
+
+
+@bp.route('/predict_from_json', methods=['POST'])
+#@login_required
+def predict_from_json():
+     json_ = request.json
+
+     #loaded_dict = json.loads(json_)
+
+     query_df = pd.DataFrame(json_, columns=json_.keys())
+
+     prediction = query_df.sum(axis=1).values
+     #prediction = np.array([2, 3])
+
+     #query_df = pd.DataFrame(json_)
+     #query = pd.get_dummies(query_df)
+     #prediction = clf.predict(query)
+     #return jsonify({'prediction': list(prediction)})
+
+     return jsonify(prediction.tolist())
